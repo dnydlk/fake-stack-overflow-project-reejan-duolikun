cde866a25195544c98304c9ad50649fe807d4c6c
dnydlk <dnydlk97@gmail.com>
2024-04-08 19:20:29 -0400

    Add Cypress e2e test for home page (mainly for setting up Github Actions)

--- client/cypress/e2e/home.cy.js
+++ client/cypress/e2e/home.cy.js
@@ -3,7 +3,17 @@ describe("Home Page 1", () => {
         cy.visit("/");
     
     })
-    it("successfully shows All Questions string", () => {
+    // it.skip: skip this test
+    it.skip("successfully shows All Questions string", () => {
+        // baseUrl is set to "http://localhost:3000" in cypress.config.js
+        // cy.visit("/"); // moved to beforeEach
+        // cy.contains("All Questions");
+        // cy.get('[data-cy-test="main-page-header"]').contains(/All Questions/i);
+        // cy.get('[data-cy-test="main-page-header"]').should("contain.text", "All Questions");
+        cy.getDateCyTest("main-page-header").should("contain.text", "All Questions");
+    });
+    // it.only: run only this test
+    it.only("successfully shows All Questions string", () => {
         // baseUrl is set to "http://localhost:3000" in cypress.config.js
         // cy.visit("/"); // moved to beforeEach
         // cy.contains("All Questions");
