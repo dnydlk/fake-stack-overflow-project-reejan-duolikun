5f8e91b1a0c809377ae12df4d8544e32458ae55c
dnydlk <dnydlk97@gmail.com>
2024-04-08 01:17:32 -0400

    Add server-side endpoints

---
+++ server/models/schema/question.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+// Schema for questions
+module.exports = mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        text: { type: String, required: true },
+        tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag" }],
+        answers: [{ type: mongoose.Schema.Types.ObjectId, ref: "Answer" }],
+        asked_by: { type: String, required: true },
+        ask_date_time: { type: Date, default: Date.now },
+        views: { type: Number, default: 0 },
+    },
+    { collection: "Question" }
+)
--- server/server.js
+++ server/server.js
@@ -1,18 +1,45 @@
 // Application server
 
+// Use express to create a server
 const express = require("express");
+// Use mongoose to connect to MongoDB
 const mongoose = require("mongoose");
+// Use cors to allow cross-origin requests
+const cors = require("cors");
 
-const { MONGO_URL, port } = require("./config");
+// Import the configuration settings
+const { MONGO_URL, port, CLIENT_URL } = require("./config");
 
+// Connect to MongoDB
 mongoose.connect(MONGO_URL);
 
 const app = express();
+
+// Allow cross-origin requests
+app.use(
+    cors({
+        credentials: true,
+        origin: [CLIENT_URL],
+    })
+);
+
+// Parse incoming requests with JSON payloads
+app.use(express.json());
 
 app.get("/", (_, res) => {
     res.send("Fake SO Server Dummy Endpoint");
     res.end();
 });
+
+// Import the controllers
+const questionController = require("./controller/question");
+const tagController = require("./controller/tag");
+const answerController = require("./controller/answer");
+
+// Use the controllers
+app.use("/question", questionController);
+app.use("/tag", tagController);
+app.use("/answer", answerController);
 
 let server = app.listen(port, () => {
     console.log(`Server starts at http://localhost:${port}`);
@@ -24,3 +51,6 @@ process.on("SIGINT", () => {
     console.log("Server closed. Database instance disconnected");
     process.exit(0);
 });
+
+// Export the server for testing
+module.exports = server;
