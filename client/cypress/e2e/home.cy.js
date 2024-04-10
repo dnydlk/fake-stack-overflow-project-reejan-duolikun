098379a6840f7c10a7833335484db5ed239a1bee
dnydlk <dnydlk97@gmail.com>
2024-04-10 00:20:40 -0400

    Refactor Cypress test for testing Github Actions in home.cy.js

--- client/cypress/e2e/home.cy.js
+++ client/cypress/e2e/home.cy.js
@@ -4,14 +4,14 @@ describe("Home Page 1", () => {
     
     })
     // it.skip: skip this test
-    it.skip("successfully shows All Questions string", () => {
-        // baseUrl is set to "http://localhost:3000" in cypress.config.js
-        // cy.visit("/"); // moved to beforeEach
-        // cy.contains("All Questions");
-        // cy.get('[data-cy-test="main-page-header"]').contains(/All Questions/i);
-        // cy.get('[data-cy-test="main-page-header"]').should("contain.text", "All Questions");
-        cy.getDateCyTest("main-page-header").should("contain.text", "All Questions");
-    });
+    // it.skip("successfully shows All Questions string", () => {
+    //     // baseUrl is set to "http://localhost:3000" in cypress.config.js
+    //     // cy.visit("/"); // moved to beforeEach
+    //     // cy.contains("All Questions");
+    //     // cy.get('[data-cy-test="main-page-header"]').contains(/All Questions/i);
+    //     // cy.get('[data-cy-test="main-page-header"]').should("contain.text", "All Questions");
+    //     cy.getDateCyTest("main-page-header").should("contain.text", "All Questions");
+    // });
     // it.only: run only this test
     it.only("successfully shows All Questions string", () => {
         // baseUrl is set to "http://localhost:3000" in cypress.config.js
