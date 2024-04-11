941afc38c8de0152d86a0f377faeb674074d40bb
dnydlk <dnydlk97@gmail.com>
2024-04-11 18:41:41 -0400

    refactor(component test): remove to test Github Actions

--- client/cypress/component/fake_so.cy.js
+++ client/cypress/component/fake_so.cy.js
@@ -3,7 +3,6 @@ import fakeStackOverflow from '../../src/components/fakestackoverflow'
 
 describe('<fakeStackOverflow />', () => {
   it("renders main title correctly", () => {
-    cy.mount(<fakeStackOverflow/>);
-    cy.get("h1").contains("All Questions"); // Assuming your Main component renders an <h1> with the title
+
   });
 })
