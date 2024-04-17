1a2f1b9d96a4898f8d48a3aeb64cbd54c3472ee5
dnydlk <dnydlk97@gmail.com>
2024-04-17 17:54:17 -0400

    add cypress component test for textarea component

--- client/src/components/main/baseComponents/textarea/index.js
+++ client/src/components/main/baseComponents/textarea/index.js
@@ -11,21 +11,30 @@ const Textarea = ({
 }) => {
     return (
         <>
-            <div className="fso-input-title">
+            <div className="fso-input-title" data-cy-test="fso-input-title">
                 {title}
                 {mandatory ? "*" : ""}
             </div>
-            {hint && <div className="fso-input-hint">{hint}</div>}
+            {hint && (
+                <div className="fso-input-hint" data-cy-test="fso-input-hint">
+                    {hint}
+                </div>
+            )}
             <textarea
                 id={id}
                 className="fso-input-input"
+                data-cy-test="fso-input-input"
                 type="text"
                 value={val}
                 onInput={(e) => {
                     setState(e.target.value);
                 }}
             />
-            {err && <div className="fso-input-error">{err}</div>}
+            {err && (
+                <div className="fso-input-error" data-cy-test="fso-input-error">
+                    {err}
+                </div>
+            )}
         </>
     );
 };
