93be0fde76a34cf91a77cdcf9215245e108f713a
dnydlk <dnydlk97@gmail.com>
2024-04-19 17:31:44 -0400

    refactor(answer.cy.js & answerPage.cy.js): import getMetaData from src/tool

--- client/cypress/component/answer.cy.js
+++ client/cypress/component/answer.cy.js
@@ -1,41 +1,6 @@
 // function getDataCyTest(selector) defined in cypress/support/commands.js
 import Answer from "../../src/components/main/answerPage/answer";
-
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-
-const getMetaData = (date) => {
-    const now = new Date();
-    const diffs = Math.floor(Math.abs(now - date) / 1000);
-
-    if (diffs < 60) {
-        return diffs + " seconds ago";
-    } else if (diffs < 60 * 60) {
-        return Math.floor(diffs / 60) + " minutes ago";
-    } else if (diffs < 60 * 60 * 24) {
-        let h = Math.floor(diffs / 3600);
-        return h + " hours ago";
-    } else if (diffs < 60 * 60 * 24 * 365) {
-        return months[date.getMonth()] + " " + getDateHelper(date) + " at " + date.toTimeString().slice(0, 8);
-    } else {
-        return (
-            months[date.getMonth()] +
-            " " +
-            getDateHelper(date) +
-            ", " +
-            date.getFullYear() +
-            " at " +
-            date.toTimeString().slice(0, 8)
-        );
-    }
-};
-
-const getDateHelper = (date) => {
-    let day = date.getDate();
-    if (day < 10) {
-        day = "0" + day;
-    }
-    return day;
-};
+import { getMetaData } from "../../src/tool";
 
 const testAnswer = {
     _id: "66220a7b7d06af88a828eec2",
