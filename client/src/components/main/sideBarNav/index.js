a077dc27348d4cfc2175ccaa52e822faff985f48
reejan6 <reejanshrestha@hotmail.com>
2024-04-21 19:24:13 -0400

    added e2e testing for profile page - profile update and activities update

--- client/src/components/main/sideBarNav/index.js
+++ client/src/components/main/sideBarNav/index.js
@@ -2,36 +2,36 @@ import "./index.css";
 import React from "react";
 
 const SideBarNav = ({ selected = "", handleQuestions, handleTags, handleProfile }) => {
-    return (
-        <div id="sideBarNav" className="fso-sideBarNav d-flex flex-column align-items-center">
-            <div
-                id="menu-profile"
-                data-cy-test="nav-tag"
-                className={`fso-menu-button ${selected === "p" ? "fso-menu-selected" : ""}`}
-                onClick={() => {
-                    handleProfile();
-                }}>
-                Profile
-            </div>
-            <div
-                id="menu-question"
-                data-cy-test="nav-question"
-                className={`fso-menu-button ${selected === "q" ? "fso-menu-selected" : ""}`}
-                onClick={() => {
-                    handleQuestions();
-                }}>
-                Questions
-            </div>
-            <div
-                id="menu-tag"
-                data-cy-test="nav-tag"
-                className={`fso-menu-button ${selected === "t" ? "fso-menu-selected" : ""}`}
-                onClick={() => {
-                    handleTags();
-                }}>
-                Tags
-            </div>
-        </div>
-    );
+	return (
+		<div id="sideBarNav" className="fso-sideBarNav d-flex flex-column align-items-center">
+			<div
+				id="menu-profile"
+				data-cy-test="nav-profile"
+				className={`fso-menu-button ${selected === "p" ? "fso-menu-selected" : ""}`}
+				onClick={() => {
+					handleProfile();
+				}}>
+				Profile
+			</div>
+			<div
+				id="menu-question"
+				data-cy-test="nav-question"
+				className={`fso-menu-button ${selected === "q" ? "fso-menu-selected" : ""}`}
+				onClick={() => {
+					handleQuestions();
+				}}>
+				Questions
+			</div>
+			<div
+				id="menu-tag"
+				data-cy-test="nav-tag"
+				className={`fso-menu-button ${selected === "t" ? "fso-menu-selected" : ""}`}
+				onClick={() => {
+					handleTags();
+				}}>
+				Tags
+			</div>
+		</div>
+	);
 };
 export default SideBarNav;
