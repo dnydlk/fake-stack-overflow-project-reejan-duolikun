9f6c1c1a7aced967306c46ab6838d9a1230f8cac
reejan6 <reejanshrestha@hotmail.com>
2024-04-21 10:49:23 -0400

    changed the userName to be username
    corrected the variable name to match what's in database and init.js

--- client/cypress/e2e/create-post.cy.js
+++ client/cypress/e2e/create-post.cy.js
@@ -67,6 +67,7 @@ describe("Home Page Should load correctly", () => {
 	it("Add a new question", () => {
 		const qTitles = [
 			"New Question Title",
+			"test question for flagging",
 			"Quick question about storage on android",
 			"Object storage for a web application",
 			"android studio save string shared preference, start activity and load the saved string",
@@ -89,5 +90,144 @@ describe("Home Page Should load correctly", () => {
 		});
 	});
 
+	it('Ask a Question with many tags', () => {
+		const qTitles = [
+			"New Question Title",
+			"test question for flagging",
+			"Quick question about storage on android",
+			"Object storage for a web application",
+			"android studio save string shared preference, start activity and load the saved string",
+			"Programmatically navigate using React router",
+		];
+		cy.getDataCyTest("ask-question-btn").click();
+		cy.getDataCyTest("loginEmail").type("test@test.com");
+		cy.getDataCyTest("loginPassword").type("q1234567");
+		cy.getDataCyTest("loginBtn").click();
+		cy.contains('Ask a Question').click();
+		cy.get('#formTitleInput').type('Test Question 1');
+		cy.get("#formTextInput").type("New Question Body New Question BodyNew Question BodyNew Question BodyNew Question BodyNew New Question BodyNew Question BodyNew Question BodyNew Question BodyNew Question BodyQuestion BodyNew New Question BodyNew Question BodyQuestion BodyNew Question BodyNew Question Body");
+		cy.get('#formTagInput').type('javascript t1 t2');
+		cy.window().then((win) => {
+			win.document.getElementById('formUsernameInput').value = 'newTestUser';
+		});
+		cy.contains('Post Question').click();
+		cy.contains('javascript');
+		cy.contains('t1');
+		cy.contains('t2');
+	});
+
+	it('Ask a Question with more than 5 tags shows error', () => {
+		const qTitles = [
+			"New Question Title",
+			"test question for flagging",
+			"Quick question about storage on android",
+			"Object storage for a web application",
+			"android studio save string shared preference, start activity and load the saved string",
+			"Programmatically navigate using React router",
+		];
+		cy.getDataCyTest("ask-question-btn").click();
+		cy.getDataCyTest("loginEmail").type("test@test.com");
+		cy.getDataCyTest("loginPassword").type("q1234567");
+		cy.getDataCyTest("loginBtn").click();
+		cy.contains('Ask a Question').click();
+		cy.get('#formTitleInput').type('Test Question 1');
+		cy.get('#formTextInput').type('Test Question 1 Text');
+		cy.get('#formTagInput').type('t1 t2 t3 t4 t5 t6');
+		cy.window().then((win) => {
+			win.document.getElementById('formUsernameInput').value = 'newTestUser';
+		});
+		cy.contains('Post Question').click();
+		cy.contains('Cannot have more than 5 tags');
+	});
+
+	it('Ask a Question with a long new tag', () => {
+		const qTitles = [
+			"New Question Title",
+			"test question for flagging",
+			"Quick question about storage on android",
+			"Object storage for a web application",
+			"android studio save string shared preference, start activity and load the saved string",
+			"Programmatically navigate using React router",
+		];
+		cy.getDataCyTest("ask-question-btn").click();
+		cy.getDataCyTest("loginEmail").type("test@test.com");
+		cy.getDataCyTest("loginPassword").type("q1234567");
+		cy.getDataCyTest("loginBtn").click();
+		cy.contains('Ask a Question').click();
+		cy.get('#formTitleInput').type('Test Question 1');
+		cy.get('#formTextInput').type('Test Question 1 Text');
+		cy.get('#formTagInput').type('t1 t2 t3 t3t4t5t6t7t8t9t3t4t5t6t7t8t9');
+		cy.window().then((win) => {
+			win.document.getElementById('formUsernameInput').value = 'newTestUser';
+		});
+		cy.contains('Post Question').click();
+		cy.contains('New tag length cannot be more than 20');
+	});
+
+	it('create a new question with a new tag and finds the question through tag', () => {
+		const qTitles = [
+			"New Question Title",
+			"test question for flagging",
+			"Quick question about storage on android",
+			"Object storage for a web application",
+			"android studio save string shared preference, start activity and load the saved string",
+			"Programmatically navigate using React router",
+		];
+		cy.getDataCyTest("ask-question-btn").click();
+		cy.getDataCyTest("loginEmail").type("test@test.com");
+		cy.getDataCyTest("loginPassword").type("q1234567");
+		cy.getDataCyTest("loginBtn").click();
+
+		// add a question with tags
+		cy.contains('Ask a Question').click();
+		cy.get('#formTitleInput').type('Test Question A');
+		cy.get("#formTextInput").type("New Question Body New Question BodyNew Question BodyNew Question BodyNew Question BodyNew New Question BodyNew Question BodyNew Question BodyNew Question BodyNew Question BodyQuestion BodyNew New Question BodyNew Question BodyQuestion BodyNew Question BodyNew Question Body");
+		cy.get('#formTagInput').type('test1-tag1 react');
+		cy.window().then((win) => {
+			win.document.getElementById('formUsernameInput').value = 'newTestUser';
+		});
+		cy.contains('Post Question').click();
+
+		// clicks tags
+		cy.contains('Tags').click();
+		cy.contains('test1-tag1').click();
+		cy.contains('1 questions');
+		cy.contains('Test Question A');
+
+		cy.contains('Tags').click();
+		cy.contains('react').click();
+		cy.contains('3 questions');
+		cy.contains('Test Question A');
+		cy.contains('Programmatically navigate using React router');
+	});
+
+	it('Ask a Question creates and accepts only 1 tag for all the repeated tags', () => {
+		const qTitles = [
+			"New Question Title",
+			"test question for flagging",
+			"Quick question about storage on android",
+			"Object storage for a web application",
+			"android studio save string shared preference, start activity and load the saved string",
+			"Programmatically navigate using React router",
+		];
+		cy.getDataCyTest("ask-question-btn").click();
+		cy.getDataCyTest("loginEmail").type("test@test.com");
+		cy.getDataCyTest("loginPassword").type("q1234567");
+		cy.getDataCyTest("loginBtn").click();
+
+		cy.contains('Ask a Question').click();
+		cy.get('#formTitleInput').type('Test Question 1');
+		cy.get("#formTextInput").type("New Question Body New Question BodyNew Question BodyNew Question BodyNew Question BodyNew New Question BodyNew Question BodyNew Question BodyNew Question BodyNew Question BodyQuestion BodyNew New Question BodyNew Question BodyQuestion BodyNew Question BodyNew Question Body");
+		cy.get('#formTagInput').type('test-tag test-tag test-tag');
+		cy.window().then((win) => {
+			win.document.getElementById('formUsernameInput').value = 'newTestUser';
+		});
+		cy.contains('Post Question').click();
+		cy.contains('test-tag').should('have.length', 1);
+		cy.contains('Tags').click();
+		cy.contains('5 Tags');
+		cy.contains('test-tag').click();
+		cy.contains('1 questions');
+	});
 
 });
