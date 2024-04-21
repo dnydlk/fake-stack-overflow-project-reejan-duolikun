e186a8eecd6e9f1e6e64826df6b0e94c33d2663a
reejan6 <reejanshrestha@hotmail.com>
2024-04-20 23:28:15 -0400

    e2e testing completed for posting question and answer
    create new post testing passed
    create new comment testing passed
    updated lenght requirement as per the testing documents for question

--- server/models/schema/user.js
+++ server/models/schema/user.js
@@ -3,25 +3,25 @@ const user = require("../user");
 
 // Schema for answers
 module.exports = mongoose.Schema(
-    {
-        _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
-        email: {
-            type: String,
-            required: "Your email is required",
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: "Your password is required",
-            select: false,
-            max: 25,
-        },
-        username: {
-            type: String,
-            max: 25,
-        },
-    },
-    { collection: "User" }
+	{
+		_id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+		email: {
+			type: String,
+			required: "Your email is required",
+			unique: true,
+			lowercase: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: "Your password is required",
+			select: false,
+			max: 25,
+		},
+		username: {
+			type: String,
+			max: 25,
+		},
+	},
+	{ collection: "User" }
 );
